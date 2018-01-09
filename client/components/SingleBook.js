3a7bf95ed4ddc1f021aae3dd52406978e5adb366
Ari Kramer <arikramer24@gmail.com>
2018-01-09 18:35:15 -0500

    single book renders

--- client/components/SingleBook.js
+++ client/components/SingleBook.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchBook } from '../store';
 
 class SingleBook extends Component {
+
+    componentDidMount() {
+        this.props.loadBooks(this.props.match.params.bookId);
+    }
+
     render() {
+        const book = this.props.singleBook;
+
         return (
-            
+            <div>
+                <img src={book.photo} />
+                <div>
+                    <div>
+                        <h3>{book.title}</h3>
+                        <h5>by {book.author}</h5>
+                    </div>
+                    <div>
+                        <p>{book.description}</p>
+                    </div>
+                </div>
+                <div>
+                    <h4>${book.price}</h4>
+                    {
+                        book.inventory > 0
+                            ?
+                            <div>
+                                <h3>In Stock.</h3>
+                                <button>Add to Cart</button>
+                            </div>
+                            :
+                            null
+                    }
+                </div>
+            </div>
         )
     }
 }
+
+const mapState = (state) => {
+    return {
+        singleBook: state.singleBook
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return {
+        loadBooks(id) {
+            dispatch(fetchBook(id));
+        }
+    }
+}
+
+export default withRouter(connect(mapState, mapDispatch)(SingleBook));
--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,7 @@ import {connect} from 'react-redux'
 import {Route, Switch, Router} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import history from './history'
-import {Main, Login, Signup, UserHome, AllBooks} from './components'
+import {Main, Login, Signup, UserHome, AllBooks, SingleBook} from './components'
 import {me} from './store'
 
 /**
@@ -24,7 +24,8 @@ class Routes extends Component {
             {/* Routes placed here are available to all visitors */}
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
-            <Route path="/books" component={AllBooks} />
+            <Route exact path="/books" component={AllBooks} />
+            <Route path="/books/:bookId" component={SingleBook} />
             {
               isLoggedIn &&
                 <Switch>
