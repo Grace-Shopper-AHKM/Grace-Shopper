ee61562befd1cda42e5fca78150cd34cb2d737a0
Ari Kramer <arikramer24@gmail.com>
2018-01-10 17:30:35 -0500

    fixed some book tests

--- server/api/books-spec.js
+++ server/api/books-spec.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 const db = require('../db');
 const app = require('../index');
 const Book = db.model('book');
+const Review = db.model('review');
+const User = db.model('user');
 
 describe('Book routes', () => {
     beforeEach(() => {
@@ -11,28 +13,28 @@ describe('Book routes', () => {
 
     describe('/api/books', () => {
         beforeEach(() => {
-                Book.create({
-                  title: 'Bossypants',
-                  photo: '/images/bossypants.jpg',
-                  sku: '4321',
-                  genre: 'biography',
-                  author: 'Tina Fey',
-                  inventory: 6,
-                  price: 12,
-                  description: 'In this hilarious book, Tina Fey reccounts her adventures from Chicago to SNL and beyond, full of wit, wisdom and how she came to "have it all"!  Liz Lemon rocks.'
-                })
-                .then(() => {
-                    Book.create({
-                      title: 'Fantastic Beasts and Where to Find Them',
-                      photo: '/images/fantastic-beasts.jpg',
-                      sku: '4322',
-                      genre: 'fiction',
-                      author: 'JK Rowling',
-                      inventory: 12,
-                      price: 17,
-                      description: 'Hogwarts comes to America.  JK Rowling does it again with a menagerie of new magical creatures!  Recently adapted into a movie feature Eddie Redmayne and Colin Farrell!  Really recommended book!'
-                    })
-                })
+            Book.bulkCreate([
+                {
+                    title: 'Bossypants',
+                    photo: '/images/bossypants.jpg',
+                    sku: '4321',
+                    genre: 'biography',
+                    author: 'Tina Fey',
+                    inventory: 6,
+                    price: 12,
+                    description: 'In this hilarious book, Tina Fey reccounts her adventures from Chicago to SNL and beyond, full of wit, wisdom and how she came to "have it all"!  Liz Lemon rocks.'
+                  },
+                  {
+                    title: 'Fantastic Beasts and Where to Find Them',
+                    photo: '/images/fantastic-beasts.jpg',
+                    sku: '4322',
+                    genre: 'fiction',
+                    author: 'JK Rowling',
+                    inventory: 12,
+                    price: 17,
+                    description: 'Hogwarts comes to America.  JK Rowling does it again with a menagerie of new magical creatures!  Recently adapted into a movie feature Eddie Redmayne and Colin Farrell!  Really recommended book!'
+                  }
+            ])
         })
 
         it('GET /api/books', () => {
@@ -40,7 +42,6 @@ describe('Book routes', () => {
                 .get('/api/books')
                 .expect(200)
                 .then(res => {
-                    // console.log(res.body)
                     expect(res.body).to.be.an('array')
                     expect(res.body[0].title).to.be.equal('Bossypants')
                     expect(res.body[1].author).to.be.equal('JK Rowling')
@@ -49,7 +50,7 @@ describe('Book routes', () => {
 
         it('GET /api/books/:bookId', () => {
             return request(app)
-                .get('/api/books/2')
+                .get('/api/books/1')
                 .expect(200)
                 .then(res => {
                     expect(res.body).to.be.an('object')
--- server/api/books.js
+++ server/api/books.js
@@ -6,19 +6,35 @@ const Review = require('../db/models/review');
 const User = require('../db/models/user')
 
 router.get('/', (req, res, next) => {
-    Book.findAll({})
-        .then(books => {
-            console.log(books)
-            res.send(books)
+    let whereObj = {};
+    console.log('query ', req.query)
+
+    if (req.query) {
+        console.log('hitit')
+        whereObj.genre = req.query.genre
+        Book.findAll({
+            where: whereObj
         })
-        .catch(next);
+            .then(books => res.json(books))
+            .catch(next);
+    } else {
+        Book.findAll({})
+            .then(books => res.json(books))
+            .catch(next);
+    }
+
 });
 
-router.get('/:bookId', (req, res, next) => {
-    Book.findById(Number(req.params.bookId))
-        .then(book => res.send(book))
-        .catch(next);
-});
+// router.get('/:genre', (req, res, next) => {
+//     Book.findAll({
+//         where: {
+//             genre: req.params.genre
+//         }
+//         // maybe implement an order by here
+//     })
+//         .then(books => res.json(books))
+//         .catch(next);
+// });
 
 router.get('/:bookId/reviews', (req, res, next) => {
     Review.findAll({
@@ -27,17 +43,14 @@ router.get('/:bookId/reviews', (req, res, next) => {
         },
         include: [{model: User}]
     })
-        .then(reviews => res.send(reviews))
+        .then(reviews => res.json(reviews))
         .catch(next);
 });
 
-router.get('/:genre', (req, res, next) => {
-    Book.findAll({
-        where: {
-            genre: req.params.genre
-        }
-        // maybe implement an order by here
-    })
-        .then(books => res.send(books))
+router.get('/:bookId', (req, res, next) => {
+    Book.findById(Number(req.params.bookId))
+        .then(book => res.json(book))
         .catch(next);
 });
+
+
