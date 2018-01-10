c1fd04cd9d080769c715038f0d65f33fb22f20b7
Han H <han@oriensus.com>
2018-01-10 17:26:14 -0500

    added thunk for cart

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import { fetchCartItems } from '../store';
+
 import CartItems from './CartItems';
 import CartTotal from './CartTotal';
 
-export default class Cart extends React.Component{
+
+class Cart extends React.Component{
     constructor(){
         super();
-        this.state = {
-            cart: [{itemId: '1', qty: 3, price: '66', desc: 'This book is damn good. you will read it 10 times' }, {itemId: '2', qty: 1, price: '77', desc: 'This book is damn good. you will read it 100 times'}, {itemId: '3', qty: 2, price: '55', desc: 'This book is damn good. you will read it 100 times'}, {itemId: '4', qty: 6, price: '55', desc: 'This book is damn good. you will read it 100 times'}]
-        }
-
+        // this.state = {
+        //     cart: [{itemId: '1', qty: 3, price: '66', desc: 'This book is damn good. you will read it 10 times' }, {itemId: '2', qty: 1, price: '77', desc: 'This book is damn good. you will read it 100 times'}, {itemId: '3', qty: 2, price: '55', desc: 'This book is damn good. you will read it 100 times'}, {itemId: '4', qty: 6, price: '55', desc: 'This book is damn good. you will read it 100 times'}]
+        // }
         this.getShoppingCartItems = this.getShoppingCartItems.bind(this);
+    }
+
+    componentDidMount() {
+        this.props.loadCart();
     }
 
     getShoppingCartItems(){
         return (
-            this.state.cart.map( (item, idx) => {
+            this.props.cart.map( (item, idx) => {
                 return <CartItems key={idx} qty={item.qty} price={item.price} desc={item.desc} />;
             })
         )          
@@ -29,10 +37,26 @@ export default class Cart extends React.Component{
                     {this.getShoppingCartItems()}
                 </div>
                 <div style={{width: '20%'}}>
-                    <CartTotal cartItems={this.state.cart}/>
+                    <CartTotal cartItems={this.props.cart}/>
                 </div>
             </div>
         )
     }
 }
 
+
+const mapState = (state) => {
+    return {
+        cart: state.cart
+    }
+}
+
+const mapDispatch = (dispatch) => {
+    return {
+        loadCart() {
+            dispatch(fetchCartItems());
+        }
+    }
+}
+
+export default withRouter(connect(mapState, mapDispatch)(Cart));
--- client/components/CartItems.js
+++ client/components/CartItems.js
@@ -9,7 +9,7 @@ export default class CartItems extends React.Component{
         return(
             <div className="shoppingcartitems">
                 <div style={{width: '10%'}}>
-                    <div style={{width: '70%', height: '70%'}}><img className='itemimages' src='/Users/poby/seniorphase/boook/public/images/fantastic-beasts.jpg' /></div>
+                    <div style={{width: '70%', height: '70%'}}><img className='itemimages' src='/images/fantastic-beasts.jpg' /></div>
                 </div>
                 <div style={{width: '50%'}}>{this.props.desc}</div>
                 <div style={{width: '20%'}}>${this.props.price}</div>
