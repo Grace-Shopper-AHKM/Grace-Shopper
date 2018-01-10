f5f5d8c9ca1632c78621fc82322bbf09519f38e2
Maxwell Legocki <mdlegocki@gmail.com>
2018-01-10 11:43:37 -0500

    added homepage component and proper links

--- client/components/main.js
+++ client/components/main.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import {withRouter, Link} from 'react-router-dom'
-import {logout} from '../store'
-
-import Cart from './Cart';
+import { connect } from 'react-redux'
+import { withRouter, Link } from 'react-router-dom'
+import { logout } from '../store'
 
 /**
  * COMPONENT
@@ -13,21 +11,27 @@ import Cart from './Cart';
  *  rendered out by the component's `children`.
  */
 const Main = (props) => {
-  const {children, handleClick, isLoggedIn} = props
+  const { children, handleClick, isLoggedIn, userId } = props
 
   return (
     <div>
-      <h1>BOILERMAKER</h1>
+      <h1>boooks</h1>
       <nav>
         {
           isLoggedIn
             ? <div>
               {/* The navbar will show these links after you log in */}
-              <Link to="/home">Home</Link>
+              <Link to="/">Home</Link>
+              <Link to="/books">Books</Link>
+              <Link to="/cart">Cart</Link>
+              <Link to={`/users/${userId}`}>My Account</Link>
               <a href="#" onClick={handleClick}>Logout</a>
             </div>
             : <div>
               {/* The navbar will show these links before you log in */}
+              <Link to="/">Home</Link>
+              <Link to="/books">Books</Link>
+              <Link to="/cart">Cart</Link>
               <Link to="/login">Login</Link>
               <Link to="/signup">Sign Up</Link>
             </div>
@@ -44,13 +48,14 @@ const Main = (props) => {
  */
 const mapState = (state) => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    userId: state.user.id
   }
 }
 
 const mapDispatch = (dispatch) => {
   return {
-    handleClick () {
+    handleClick() {
       dispatch(logout())
     }
   }
