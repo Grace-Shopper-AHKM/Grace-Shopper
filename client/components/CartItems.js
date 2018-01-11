5638fdfccf896919b1d0771438b4ad8c202397d6
Han H <han@oriensus.com>
2018-01-11 14:22:15 -0500

    partial code for qty update

--- client/components/CartItems.js
+++ client/components/CartItems.js
@@ -9,15 +9,33 @@ const con = console.log;
 export default class CartItems extends React.Component{
     constructor(){
         super();
+        this.getUpdatedQty = this.getUpdatedQty.bind(this);
+        this.updateItemQty = this.updateItemQty.bind(this);
     }
 
     deleteItemFromCart(itemid){
         let newCart = [];
-        store.getState().cart.map((item => {
+        store.getState().cart.forEach((item => {
             if( item.itemId !==  itemid)
                 newCart.push(item);
         }))
         store.dispatch(getCart(newCart));
+    }
+
+    updateItemQty(event){
+        con('event', event.target.value);
+        con('props', this.props);
+        // let newCart = store.getState().cart.map((item => {
+        //     if( item.itemId === itemid)
+        //         item.qty = newQty;
+        //     return item;
+        // }))
+        // store.dispatch(getCart(newCart));
+    }
+
+    getUpdatedQty(event, itemid){
+        con('yyyyyy', event.target.value);
+        con('ssssss', itemid)
     }
 
     render(){
@@ -28,15 +46,16 @@ export default class CartItems extends React.Component{
                 </div>
                 <div className='itemdescription' style={{width: '50%'}}>
                     {this.props.desc}
-                    
                     <input type="submit" value="Delete" onClick={() => this.deleteItemFromCart(this.props.itemid)}></input>
-
-
                 </div>
                 <div style={{width: '20%'}}>${this.props.price}</div>
+
+
+
+
                 <div style={{width: '20%'}}>
-                    <input className='qtybox' type='text' maxLength="3" defaultValue={this.props.qty} />
-                    <button>Update</button>
+                        <input className='qtybox' type='text' maxLength="3" defaultValue={this.props.qty} onChange={(event) => this.getUpdatedQty(event, this.props.itemid)}/>
+                        <button onClick={() => this.updateItemQty(this.props.itemid) }  >Update</button>
                 </div>
             </div>
         )
