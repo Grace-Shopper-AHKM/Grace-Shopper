63d758e0b9c85d3cbd6090e71e7dc2f62b7f928d
Han H <han@oriensus.com>
2018-01-11 11:31:55 -0500

    delete item from cart

--- client/components/CartItems.js
+++ client/components/CartItems.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import {withRouter} from 'react-router-dom';
+import {connect} from 'react-redux';
+import { getCart } from '../store';
+import store from '../store';
+
+const con = console.log;
 
 export default class CartItems extends React.Component{
     constructor(){
         super();
     }
 
+    deleteItemFromCart(itemid){
+        let newCart = [];
+        store.getState().cart.map((item => {
+            if( item.itemId !==  itemid)
+                newCart.push(item);
+        }))
+        store.dispatch(getCart(newCart));
+    }
+
     render(){
         return(
             <div className="shoppingcartitems">
-                <div style={{width: '10%'}}>
+                <div className='itemimagecontainer' style={{width: '10%'}}>
                     <div style={{width: '70%', height: '70%'}}><img className='itemimages' src='/images/fantastic-beasts.jpg' /></div>
                 </div>
-                <div style={{width: '50%'}}>{this.props.desc}</div>
+                <div className='itemdescription' style={{width: '50%'}}>
+                    {this.props.desc}
+                    
+                    <input type="submit" value="Delete" onClick={() => this.deleteItemFromCart(this.props.itemid)}></input>
+
+
+                </div>
                 <div style={{width: '20%'}}>${this.props.price}</div>
-                <div style={{width: '20%'}}>{this.props.qty}</div>
+                <div style={{width: '20%'}}>
+                    <input className='qtybox' type='text' maxLength="3" defaultValue={this.props.qty} />
+                    <button>Update</button>
+                </div>
             </div>
         )
     }
--- client/store/getCart.js
+++ client/store/getCart.js
@@ -1,7 +1,10 @@
 
 const GET_CART = 'GET_CART';
+const DELETE_ITEM = 'DELETE_ITEM';
+
 const cart = [{itemId: '1', qty: 3, price: '66', desc: 'This book is damn good. you will read it 10 times' }, {itemId: '2', qty: 1, price: '77', desc: 'This book is damn good. you will read it 100 times'}, {itemId: '3', qty: 2, price: '55', desc: 'This book is damn good. you will read it 100 times'}, {itemId: '4', qty: 6, price: '55', desc: 'This book is damn good. you will read it 100 times'}]
 
+//// ACTIONS
 export function getCart(cart) {
     return {
         type: GET_CART,
@@ -9,12 +12,27 @@ export function getCart(cart) {
     }
 }
 
+export function deleteItem(cart){
+    return {
+        type: DELETE_ITEM,
+        cart
+    }
+}
+
+//THUNKS
 export function fetchCartItems() {
     return function thunk(dispatch) {
         return dispatch(getCart(cart))
     }
 }
 
+export function deleteItemFromCart(){
+    return function thunk(dispatch){
+        return dispatch()
+    }
+}
+
+//// REDUCER
 export default function cartReducer(state = [], action) {
     switch (action.type) {
         case GET_CART:
