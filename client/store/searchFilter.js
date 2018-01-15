93fa6e43a5db69aca4809beb6dbec550a0cfbc5d
Maxwell Legocki <mdlegocki@gmail.com>
2018-01-15 16:05:15 -0500

    search-bar re-optimized

--- client/store/searchFilter.js
+++ client/store/searchFilter.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const GET_FILTERED_BOOKS = 'GET_FILTERED_BOOKS';
+const TOTAL_BOOKS = 'TOTAL_BOOKS';
 
-export function getFilteredBooks(books, searchTerm) {
+export function fetchFilteredBooks(books, searchTerm) {
     return {
         type: GET_FILTERED_BOOKS,
         books,
@@ -10,9 +11,27 @@ export function getFilteredBooks(books, searchTerm) {
     }
 };
 
-export default function booksReducer(state = [], action) {
+export function getTotalBooks(books) {
+    return {
+        type: TOTAL_BOOKS,
+        books
+    }
+};
+
+export function fetchTotalBooks() {
+    return function thunk(dispatch) {
+        return axios.get('/api/books')
+            .then(res => res.data)
+            .then(books => dispatch(getTotalBooks(books)))
+            .catch(console.error);
+    }
+}
+
+export default function searchFilter(state = [], action) {
     switch (action.type) {
-        case GET_BOOKS:
+        case GET_FILTERED_BOOKS:
+            return action.books.filter((book) => book.title.toLowerCase().includes(action.searchTerm.toLowerCase()));
+        case TOTAL_BOOKS:
             return action.books;
         default:
             return state;
