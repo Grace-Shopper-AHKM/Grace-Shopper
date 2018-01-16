527bcdb9cf58f40671e5186cb6ad3b351b95feaa
Ari Kramer <arikramer24@gmail.com>
2018-01-16 18:01:48 -0500

    styled cart

--- client/store/orders.js
+++ client/store/orders.js
@@ -3,7 +3,6 @@ import history from '../history'
 
 export function addOrder(order) {
     return function thunk(dispatch) {
-      console.log(order);
       return axios.post('/api/orders/add-order', order)
         .then(history.push('/books'))
         .catch(console.error);
